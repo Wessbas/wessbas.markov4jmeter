44dfd9f9daffc49e0d1a0fa7bb12c2f1392d6db5
Andre van Hoorn <van.hoorn@informatik.uni-stuttgart.de>
2011-06-30 16:35:23 +0000

    added 3rd-party compile-time dependencies to repository and source archives

--- src/net/voorn/markov4jmeter/util/Markov4JMeterVersion.java
+++ src/net/voorn/markov4jmeter/util/Markov4JMeterVersion.java
@@ -33,7 +33,7 @@ public class Markov4JMeterVersion {
 	 * JMeterUtils This ensures that JMeterUtils always gets the correct
 	 * version, even if it is not re-compiled during the build.
 	 */
-	private static final String VERSION = "1.0.20081009";
+	private static final String VERSION = "1.0.20110630";
 
 	static final String COPYRIGHT = "Copyright (c) 2007 Andre van Hoorn";
 
